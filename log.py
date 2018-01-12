af05054513924aed8cc3610363b5b542ae2af579
lichengmin <lcmasdf@126.com>
2018-01-12 11:57:57 +0800

    添加了log.py

---
+++ log.py
@@ -0,0 +1,15 @@
+error_log_file = 'error_record.txt'
+success_log_file = 'success_record.txt'
+
+
+def error_log(username, reason):
+	with open(error_log_file, 'a') as file:
+		file.write(username+'  login error  '+reason)
+		file.write('\n')
+		file.close()
+
+def success_log(username, score):
+	with open(success_log_file, 'a') as file:
+		file.write(username+'  has finish the test, score :  '+score)
+		file.write('\n')
+		file.close()
